495c8ee769bedf51c7e370ace5aab8e23b0d2175
Andre Menezes <djdedemenezes@gmail.com>
2022-06-30 20:37:52 -0300

    refactoring solid

--- scripts/init_calendar.js
+++ scripts/init_calendar.js
@@ -1,25 +1,43 @@
+const addCheckbox = (day) => {
+  day.style.transition = "all .5s ease-in-out";
+  console.log(day)
+  const klass = day.querySelector('.calendar-day-name')
+  const week = day.parentElement.previousElementSibling.innerText
+  const value = klass.innerText.trim();
+  const checkbox = `<input type='checkbox' data-week='${week.replace(" ", "_")}' value='${value}' style='margin: 0 24px;'>`
+  day.insertAdjacentHTML('beforeend', checkbox)
+}
+
 const displayCheckboxes = () => {
   days = document.querySelectorAll('.calendar-day')
-
-  days.forEach((day) => {
-    // day.style.transition = 'all 1s ease-in-out;'
-    day.style.transition = "all 1s ease-in-out";
-    console.log(day)
-    id = day.dataset.id
-    klass = day.querySelector('.calendar-day-name')
-    week = day.parentElement.previousElementSibling.innerText
-    value = klass.innerText.trim();
-    checkbox = `<input type='checkbox' data-week='${week.replace(" ", "_")}' value='${value}' style='margin: 0 24px;'>`
-    day.insertAdjacentHTML('beforeend', checkbox)
-  })
+  days.forEach(addCheckbox)
 }
 
-const chosenKlasses = () => {
+const changeBackgroundColor = (event) => {
+  // event.currentTarget.style.backgroundColor = '#611365'
+  dayDiv = event.currentTarget.parentElement;
+  const bgPurple = "rgba(180, 26, 188, 0.404)";
+  const bgDefault = "#F7F7F7";
+  if (dayDiv.classList.contains('bg-purple')) {
+    dayDiv.style.backgroundColor = bgDefault
+    dayDiv.classList.remove('bg-purple')
+  } else {
+    dayDiv.style.backgroundColor = bgPurple
+    dayDiv.classList.add('bg-purple')
+  };
+  // console.log(event.currentTarget.parentElement.style.backgroundColor)
+  // event.currentTarget.parentElement.style.backgroundColor = '#F7F7F7'
+}
+
+const changeCheckbox = (input) => {
+  input.addEventListener('change', changeBackgroundColor)
+}
+
+
+const styleDayDivWhenSelectCheckbox = () => {
   const inputs = document.querySelectorAll('input[type="checkbox"]');
-  return Array.from(inputs)
-    .filter(input => input.checked)
-    .map(input => input.value);
-}
+  inputs.forEach(changeCheckbox);
+};
 
 const addCopyToClipboardButton = () => {
   const navMenuSection = document.querySelector('.navbar-collection.d-flex.flex-row-reverse.align-items-center')
@@ -27,9 +45,11 @@ const addCopyToClipboardButton = () => {
   return document.getElementById('message');
 }
 
-const copyMessage = () => {
-  const message = localStorage.getItem('message')
-  navigator.clipboard.writeText(message)
+const chosenKlasses = () => {
+  const inputs = document.querySelectorAll('input[type="checkbox"]');
+  return Array.from(inputs)
+    .filter(input => input.checked)
+    .map(input => input.value);
 }
 
 const buildCalendar = () => {
@@ -44,35 +64,21 @@ const buildMessage = (calendar) => {
   return Object.entries(calendar).map(keyValue => keyValue.join(' - ')).join("\n")
 }
 
+
+const copyMessage = () => {
+  const message = localStorage.getItem('message')
+  navigator.clipboard.writeText(message)
+}
+
 const createList = () => {
-  // calendar = buildCalendar();
-  // message = buildMessage(calendar);
-  // localStorage.setItem('message', message);
-  // copyMessage()
   localStorage.setItem('message', buildMessage(buildCalendar()));
   copyMessage()
 }
 
+
 const initCalendar = () => {
   displayCheckboxes();
-  const inputs = document.querySelectorAll('input[type="checkbox"]');
-  inputs.forEach((input) => {
-    input.addEventListener('change', (event) => {
-      // event.currentTarget.style.backgroundColor = '#611365'
-      dayDiv = event.currentTarget.parentElement
-      const bgPurple = "rgba(180, 26, 188, 0.404)";
-      const bgDefault = "#F7F7F7";
-      if(dayDiv.classList.contains('bg-purple')) {
-        dayDiv.style.backgroundColor = bgDefault
-        dayDiv.classList.remove('bg-purple')
-      } else {
-        dayDiv.style.backgroundColor = bgPurple
-        dayDiv.classList.add('bg-purple')
-      }
-      // console.log(event.currentTarget.parentElement.style.backgroundColor)
-      // event.currentTarget.parentElement.style.backgroundColor = '#F7F7F7'
-    })
-  })
+  styleDayDivWhenSelectCheckbox()
   copyButton = addCopyToClipboardButton()
   // cada input
   copyButton.addEventListener('click', createList)
