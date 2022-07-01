d95026d07d61111c300b1c56e7a6b96954dde40c
Andre Menezes <djdedemenezes@gmail.com>
2022-07-01 09:41:06 -0300

    still working, extractingeach metod into support or components

---
+++ src/components/hide_and_seek.js
@@ -0,0 +1,53 @@
+const displayCopyIcon = () => {
+  const icon = getCopyIcon();
+  icon.classList.remove('d-none')
+}
+
+const hideCopyIcon = () => {
+  const icon = getCopyIcon();
+  icon.classList.add('d-none')
+}
+
+const displayStartTip = () => {
+  const tip = geStartTip()
+  tip.classList.remove('d-none')
+}
+
+const hideStartTip = () => {
+  const tip = geStartTip()
+  tip.classList.add('d-none')
+}
+
+const displayCopyingTip = () => {
+  const copyingTip = getCopyingTip();
+  copyingTip.classList.remove('d-none');
+}
+
+const hideCopyingTip = () => {
+  const copyingTip = getCopyingTip();
+
+  copyingTip.classList.add('d-none');
+}
+
+const displayResetIcon = () => {
+  const resetIcon = document.querySelector('#reset-icon');
+  resetIcon.classList.remove('d-none')
+}
+
+const hideResetIcon = () => {
+  const resetIcon = document.querySelector('#reset-icon');
+  resetIcon.classList.add('d-none')
+}
+
+const displayResetTip = () => {
+  const resetTip = document.querySelector('#reset-tip');
+  resetTip.classList.remove('d-none')
+}
+
+const hideResetTip = () => {
+  const resetTip = document.querySelector('#reset-tip');
+  resetTip.classList.add('d-none')
+}
+const hideAndSeek = () => {
+
+}
---
+++ src/support/get_start_tip.js
@@ -0,0 +1,3 @@
+const geStartTip = () => {
+  return document.querySelector('#start-tip');
+}
