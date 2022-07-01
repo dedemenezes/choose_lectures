bd0e8f67cc70d2a601be0e9a21f4bfe0e8c142d9
Andre Menezes <djdedemenezes@gmail.com>
2022-07-01 00:49:56 -0300

    using lw classes to style btn

--- src/add_copy_to_clipboard_button.js
+++ src/add_copy_to_clipboard_button.js
@@ -1,6 +1,14 @@
 const addCopyToClipboardButton = () => {
   const navMenuSection = document.querySelector('.navbar-collection.d-flex.flex-row-reverse.align-items-center')
-  const btn = `<button id="message" class="btn-ext animated-bg">Copy classes to clipboard</button>`
+  const content = `
+    <div class='menu-trigger-content'>
+      <span class='menu-trigger-content-label' id='btn-copy-text'>Copy list</span>
+      <span class='menu-trigger-content-city d-none' id='ctrl-v'>Now, ctrl + v =)</span>
+      <span class='menu-trigger-content-city' id='copy-tip'>to clipboard</span>
+    </div>
+    <i class="fas fa-copy text-white" id='copy-icon'></i>
+  `
+  const btn = `<button id="message" class="menu-trigger animated-bg">${content}</button>`
   navMenuSection.insertAdjacentHTML('beforeend', btn)
 };
 
